c1db21cc248123a3947b2d52316d0cce235410c2
Vara33125 <vincent.kherfallah@gmail.com>
2024-05-25 22:01:13 +0200

    Sprint2_Admin_backoffice_creation

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+#[Route('/admin')]
+class AdminController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_home', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->render('pages/admin/home/index.html.twig');
+    }
+}
